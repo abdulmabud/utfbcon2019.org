d3cd1f984c07735845f5e8b23331b9171ae862b0
abdulmabud <abdulmabudm@gmail.com>
2019-10-01 17:10:30 +0600

    Update slider image

--- index.php
+++ index.php
@@ -4,30 +4,44 @@
               <li data-target="#carouselId" data-slide-to="0" class="active"></li>
               <li data-target="#carouselId" data-slide-to="1"></li>
               <li data-target="#carouselId" data-slide-to="2"></li>
+              <li data-target="#carouselId" data-slide-to="3"></li>
           </ol>
           <div class="carousel-inner" role="listbox">
               <div class="carousel-item active">
                   <img src="image/Slider/1.png" alt="First slide" width="100%">
-                  <div class="carousel-caption d-none d-md-block">
-                    
+                  <div class="carousel-caption d-none d-md-block" style="padding-bottom: 10%;">
+                     
+                  <h1 style="font-size: 300%">
+                  Who Should Attend
+                  </h1>
+                  <h2>
+                      This educational program is intended for physicians, urologists, urologic oncologists, surgical oncologists, medical oncologists and all other allied health care professionals interested in the treatment of urologic malignancies.
+                      </h2>
+                   
                   </div>
               </div>
               <div class="carousel-item">
                   <img src="image/Slider/2.png" alt="Second slide" width="100%">
-                  <div class="carousel-caption d-none d-md-block">
-                     
+                  <div class="carousel-caption d-none d-md-block" style="padding-bottom: 10%;">
+                  <h2>
+                    The 2nd UTFB Uro-Oncology Conference 2019 has been issuing wide range of topics over the years. This conference provides a forum for presenting original unpublished data and sharing ideas for uro-oncological innovation. Although the main topic of the conference will be prostate and renal cancer, contemporary management of other urological malignancies will also be discussed.
+                    </h2>
                   </div>
               </div>
               <div class="carousel-item">
                   <img src="image/Slider/3.png" alt="Third slide" width="100%">
-                  <div class="carousel-caption d-none d-md-block">
-                    
+                  <div class="carousel-caption d-none d-md-block" style="padding-bottom: 10%;">
+                    <h2>
+                    Urologists and affiliated professionals attending the conference will be able to review innovative techniques and scientific advances in the field of urological oncology, especially on prostate and renal cancer.
+                     </h2>
                   </div>
               </div>
               <div class="carousel-item">
                   <img src="image/Slider/4.png" alt="Third slide" width="100%">
-                  <div class="carousel-caption d-none d-md-block">
-                     
+                  <div class="carousel-caption d-none d-md-block" style="padding-bottom: 10%;">
+                    <h2>
+                    They will also find the chance to communicate, collaborate and network with representatives of a large international audience - medical professionals and national urological societies.
+                    </h2>
                   </div>
               </div>
           </div>
